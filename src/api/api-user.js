545514d4c628d6531d9593e046457fc7713fb366
new <aji@gmail.com>
2020-12-01 00:43:17 +0100

    make it better

--- src/api/api-user.js
+++ src/api/api-user.js
@@ -1,5 +1,5 @@
 //const link = 'http://localhost:8080';
-const link = 'https://ochbackend.herokuapp.com';
+const link = 'https://ochback.herokuapp.com';
 
 const read = (params) => {
   return fetch(link + '/api/admin/' + params.userId, {
